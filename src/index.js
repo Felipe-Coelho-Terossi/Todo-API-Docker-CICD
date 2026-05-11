d54d7c6c5b60f1c94d82c427419c09b70b366b3d
Felipe-Coelho-Terossi <felipeterossi2@gmail.com>
2026-05-10 23:52:06 -0300

    fix: adiciona cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.use('/todos', require('./routes/todos'));
@@ -9,4 +11,4 @@ app.use('/todos', require('./routes/todos'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor na porta ${PORT}`));
 
-module.exports = app; // necessário pros testes
+module.exports = app;
